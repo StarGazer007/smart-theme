812d14c1f6877cb8d688d4cfb23cdc6994358e5c
Lisa Rodgers <lisa@spyvsspycomputers.com>
2018-12-06 06:47:48 -0500

    names update

--- loop.php
+++ loop.php
@@ -29,7 +29,7 @@
 
 	<!-- article -->
 	<article>
-		<h2><?php _e( 'Sorry, nothing to display.', 'taoist' ); ?></h2>
+		<h2><?php _e( 'Sorry, nothing to display.', 'smart-theme' ); ?></h2>
 	</article>
 	<!-- /article -->
 
